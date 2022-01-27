2e5c272bace7113f03b2ef9f5caa7f904bc5f697
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2022-01-26 23:13:11 -0500

    Update CreateAccountForm.js

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -2,13 +2,19 @@
  * Класс CreateAccountForm управляет формой
  * создания нового счёта
  * */
-class CreateAccountForm extends AsyncForm {
+ class CreateAccountForm extends AsyncForm {
   /**
    * Создаёт счёт с помощью Account.create и закрывает
    * окно в случае успеха, а также вызывает App.update()
    * и сбрасывает форму
    * */
   onSubmit(data) {
-
+     Account.create(data, (err, response) => {
+       if(response.success) {
+         App.getModal('createAccount').close();
+         App.update();
+         this.element.reset();
+       }
+     })
   }
 }
