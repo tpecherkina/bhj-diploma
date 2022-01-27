decfbab5bf809920603d6a736786ac57ccb707ac
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2022-01-26 23:17:11 -0500

    Update RegisterForm.js

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -2,7 +2,7 @@
  * Класс RegisterForm управляет формой
  * регистрации
  * */
-class RegisterForm extends AsyncForm {
+ class RegisterForm extends AsyncForm {
   /**
    * Производит регистрацию с помощью User.register
    * После успешной регистрации устанавливает
@@ -10,6 +10,14 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.register(data, (err, response) => {
+      
+      if (response && response.success) {
+        this.element.reset();
+        App.setState( 'user-logged' );
+        const modalWindow = App.getModal('register');
+        modalWindow.close();
+      }
+    });
   }
 }
